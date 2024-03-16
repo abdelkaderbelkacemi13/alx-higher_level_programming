2d6ba99da08ab67cd75732d6dd513e2166f5bc3e
Abdelkader Belkacemi <140658748+abdelkaderbelkacemi13@users.noreply.github.com>
2024-03-16 15:03:12 +0100

    Delete 0x03-python-data-structures directory

--- 0x03-python-data-structures/0-print_list_integer.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for ilist in my_list:
-        print("{:d}".format(ilist))
--- 0x03-python-data-structures/5-no_c.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    new_str = ''
-    for cc in my_string:
-        if cc == 'c' or cc == 'C':
-            continue
-        else:
-            new_str += cc
-    return (new_str)
