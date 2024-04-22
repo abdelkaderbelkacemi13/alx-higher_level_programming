78f8f0a90dbdf219406770b4e13fcd624f73ec98
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-04-22 14:17:30 -0700

    python

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Class Base model"""
+
+
+class Base:
+    """Class Base representation"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
