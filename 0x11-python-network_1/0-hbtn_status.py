4c164fc0956111f08eec3b810e55a5b612af22fc
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-07-05 15:23:34 +0100

    task

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""a Python script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resopnse:
+        body_respo = resopnse.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body_respo)))
+    print("\t- content: {}".format(body_respo))
+    print("\t- utf8 content: {}".format(body_respo.decode("utf-8")))
--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""a Python script that takes in a URL,
+sends a request to the URL and displays the value of the X-Request-Id 
+variable found in the header of the response"""
+import sys
+import urllib.request
+
+
+if __name__ == "__main__":
+    URL =  sys.argv[1]
+    with urllib.request.urlopen(urllib.request.Request(URL)) as response:
+        print(dict(response.headers).get("X-Request-Id"))
