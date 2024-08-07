0d580137908b8becd02c1185f11e52316dd46738
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-07 16:13:20 -0700

    task #3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" This script takes in a URL sends a request to the URL 
+""" This script takes in a URL sends a request to the URL
     and displays the body of the response
 """
 # import the nececery modules
