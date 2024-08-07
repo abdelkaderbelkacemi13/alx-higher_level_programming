bb0fa862058b0ced2ffba8342b865b2d9125191d
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-07 21:17:11 +0100

    pyc

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,12 +2,13 @@
 """This script sends a POST request to a given URL with an email parameter
     and displays the body of the response
 """
-#import the nececery modules
+# import the nececery modules
 import sys
-import urllib.parse, urllib.request
+import urllib.parse
+import urllib.request
 
 if __name__ == "__main__":
-    #the first argument is the URL
+    # the first argument is the URL
     url = sys.argv[1]
     email_val = {"email": sys.argv[2]}
     data = urllib.parse.urlencode(email_val).encode("ascii")
