25ee7e7ee5e74ed00623a71f4141bce057f7339d
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-08 00:37:57 +0100

    task #4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""This python script that fetches https://alx-intranet.hbtn.io/status"""
+# import the nececery modules
+import requests
+
+
+if __name__ == "__main__":
+    # Send an HTTP GET request to URL
+    http_response = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(http_response.text)))
+    print("\t- content: {}".format(http_response.text))
