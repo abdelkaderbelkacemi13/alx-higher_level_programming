14ecfa489b5fbb0fd341e88fabe61a1d7b9f18b6
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-10 16:18:45 -0700

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""this Python script that takes in a URL and an email
+    and displays the body of the response.
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    given_url = sys.argv[1]
+    email_value = {'email': sys.argv[2]}
+
+    # req = request
+    req = requests.get(given_url, data=email_value)
+    # Print the text content of the server's response
+    print(req.text)
