51c0ae7d9fa62a96e53fac830c7168b243de0031
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-10 16:03:50 -0700

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""this Python script that takes in a URL
+     and displays the value of the variable X-Request-Id
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    given_url = sys.argv[1]
+
+    # req = request
+    req = requests.get(given_url)
+    print(req.headers.get("X-Request-Id"))
