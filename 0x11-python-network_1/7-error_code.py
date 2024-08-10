7a845d7dfddd96a0cbde15f0f62fa3b7055fe42d
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-10 16:57:37 -0700

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""this Python script that takes in a URL
+     and displays the body of the response
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    given_url = sys.argv[1]
+
+    # req = request
+    req = requests.get(given_url)
+    # Check if the response status code indicates an error
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
