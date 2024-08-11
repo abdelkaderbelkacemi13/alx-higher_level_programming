16f60da8b13529e4905c1d9cadd5e4e6c7f5a6fe
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-10 17:18:34 -0700

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" """
+"""
+this python script takes in a letter
+and sends a POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter.
+"""
 import sys
 import requests
 
