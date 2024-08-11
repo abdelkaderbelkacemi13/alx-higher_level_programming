cee3c8018f5cdde8c2dc0000096a3148a1a3d0cb
abdelkaderbelkacemi13 <belkacemi.abdelkader54@gmail.com>
2024-08-10 17:14:14 -0700

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+""" """
+import sys
+import requests
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
+
+    # req =request
+    req = requests.post("http://0.0.0.0:5000/search_user", data=payload)
+    try:
+        respon = req.json()
+        if respon == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(respon.get("id"), respon.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
